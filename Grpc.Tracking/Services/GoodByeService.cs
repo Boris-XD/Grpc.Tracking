db5d8360c2107b52b510d977099ab58e1a754842
Boris Sea <seaborisrc@gmail.com>
2023-07-04 21:44:52 -0400

    Add Grpc project with API

---
+++ Grpc.Tracking/Services/GoodByeService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace Grpc.Tracking.Services
+{
+    public class GoodByeService : GoodBye.GoodByeBase
+    {
+        public override Task<ByeReply> SayGoodBye(ByeRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ByeReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
